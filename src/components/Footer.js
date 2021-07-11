0dfe36b43a180fa6f7acaab732496df8cb5c2882
Muhammad Talha waqar <talhawaqar007@gmail.com>
2021-07-11 16:57:24 +0500

    update form design

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="footer text-center container-flued bg-white">
+    <h3>Made by Muhammad Talha Waqar</h3>
+  </div>
+);
+
+export default Footer;
